1ee5f219e4293e3370012fa62116a8a154a3dc67
Philippe Dixon <philippe@cpxusacsp.onmicrosoft.com>
2018-06-15 14:36:02 -0400

    finished "Displaying a List" section of tutorial" "

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
-  constructor() { }
+  selectedHero: Hero;
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
